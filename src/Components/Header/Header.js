856f8e9d3da2bdb27d5180bf7871e1886d0d012a
developeremdad <developeremdad@gmail.com>
2021-09-26 11:18:50 +0600

    complete header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
+        <div className="header-container text-center p-2">
+            <h2 className="header-title">ORGANIGING A FASHION FESTIVAL</h2>
+            <p className="header-description text-light fs-5">Collection of world top models for fashion festivals</p>
+            <h4 className="">Total Budget: 15 Million</h4>
         </div>
     );
 };
